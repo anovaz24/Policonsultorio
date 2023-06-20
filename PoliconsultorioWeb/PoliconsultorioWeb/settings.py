35ee031534d4139e9bc1d06c415b731278c64f21
Alicia Vazquez <anovaz@yahoo.com>
2023-06-20 01:10:23 -0300

    .

--- PoliconsultorioWeb/PoliconsultorioWeb/settings.py
+++ PoliconsultorioWeb/PoliconsultorioWeb/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'PoliconsultorioWeb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Policonsultorio',
+        'NAME': 'policonsultorio',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'SuperUserPostgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
